767c9f4a3925361832a26a2c2508e0e17474d96f
OtomeSound <s890081tonyhsu@gmail.com>
2021-11-04 23:07:51 +0800

    Update css/js version. convert questions from spreadsheet to json files.
    Because quiz-wiz.com is down, we cannot load dynamically from the
    website.
    Although we have some questions which is backup from that website, we
    convert them into json files.
    If there are some website which provide the management of these
    questions, we will switch back to update dynamically.

--- main.js
+++ main.js
@@ -1,137 +1,92 @@
-var dataw = [], datac = [];
-//var gs = "https://spreadsheets.google.com/feeds/list/1StkcQvE-2XfEPZCeV2XWdzXqqs-Ib1ZgU98tzdFKzbI/1/public/values?alt=json";
-var gsw = "https://spreadsheets.google.com/feeds/list/1StkcQvE-2XfEPZCeV2XWdzXqqs-Ib1ZgU98tzdFKzbI/1/public/values?alt=json";
-var gsc = "https://spreadsheets.google.com/feeds/list/1StkcQvE-2XfEPZCeV2XWdzXqqs-Ib1ZgU98tzdFKzbI/2/public/values?alt=json";
-$.ajax(gsw)
-  .done(function(r){
-    r.feed.entry.forEach(function(s){
-      var q = s['gsx$問題']['$t'].toLowerCase();
-      var a = s['gsx$答え']['$t'].split('');
-      dataw.push({q: q, a: a});
-    });
-    $('#loadw').text(dataw.length);
-    $('#wquesPart').on('keyup', wkeyupCallback);
-    $('#wplayerPos input[type=radio]').on('change', wkeyupCallback);
+var qte = [], multi = [];
+
+var qte_url   = "./json/qte_final.json";
+var multi_url = "./json/multi_final.json";
+
+fetch(qte_url)
+  .then(response => response.json())
+  .then(json => {
+    qte = json.map(row => ({ q: row.Question, a: row.Answer.split('') }));
+    document.querySelector('#qte_cnt')
+            .innerText = qte.length;
+    document.querySelector('#qte_input')
+            .addEventListener('keyup', qteKeyupCallback);
+    document.querySelectorAll('#qte_pos input[type=radio]')
+            .forEach(radio => {
+              radio.addEventListener('change', qteKeyupCallback);
+            });
   })
-  .fail(function(e){
-    console.error(e);
-  });
+  .catch(e => console.error(e));
 
-$.ajax(gsc)
-  .done(function(r){
-    r.feed.entry.forEach(function(s){
-      var q = s['gsx$問題答え']['$t'].toLowerCase();
-      var a = s['gsx$答え']['$t'].split('／');
-      datac.push({q: q, a: a});
-    });
-    $('#loadc').text(datac.length);
-    $('#cquesPart').on('keyup', ckeyupCallback);
-    $('#cplayerPos').on('change', ckeyupCallback);
+fetch(multi_url)
+  .then(response => response.json())
+  .then(json => {
+    multi = json.map(row => ({ q: row.Question, a: row.Answer.split('　') }));
+    document.querySelector('#multi_cnt')
+            .innerText = multi.length;
+    document.querySelector('#multi_input')
+            .addEventListener('keyup', multiKeyupCallback);
   })
-  .fail(function(e){
-    console.error(e.responseJSON);
-  });
+  .catch(e => console.error(e));
 
-$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-  $('.tab-pane').each(function(){$(this).removeClass('active')});
-  $('.navbar-toggler:visible').click();
-});
+function search4Result (val, data) {
+  const keywords = val.split(' ').reduce((last, keyword) => {
+    if (keyword.length === 0 || last.indexOf(keyword) >= 0) return last;
 
-$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-  switch($(e.target).attr('id')){
-    case 'qtew-tab':
-      $('#wquesPart').val('').trigger('propertychange');
-      $('#wresList').html('');
-      $('#wplayerPos label:first-child').click().addClass('active');
-      break;
-    case 'qtec-tab':
-      $('#cquesPart').val('').trigger('propertychange');
-      $('#cresList').html('');
-      break;
-    default:
-      break;
-  }
-});
+    last.push(keyword);
+    return last;
+  }, []);
 
-$('input[type="text"]').on('input propertychange', function() {
-  var $this = $(this);
-  var visible = Boolean($this.val());
-  $this.siblings('.input-clear').toggleClass('hidden', !visible);
-}).trigger('propertychange');
+  const result = data.filter(row => keywords.every(keyword => row.q.indexOf(keyword) >= 0));
 
-$('.input-clear').click(function() {
-  $(this).siblings('input[type="text"]').val('')
-    .trigger('propertychange').keyup().focus();
-});
-
-function search4Result(val, data){
-  var i, j, len, res;
-  try {
-    if (val.split(" ").length > 1) {
-      val = val.split(" ");
-      val = _.uniqBy(val);
-      for(i = j = 0, len = val.length; j < len; i = ++j) {
-        var v = val[i];
-        if(v === "") delete[i];
-      }
-      res = _.filter(data, function(o){
-        var f = true;
-        val.forEach(function(v){
-          if(o.q.indexOf(v) === -1) f = false;
-        })
-        return f;
-      });
-    } else {
-      res = _.filter(data, function(o){
-        return o.q.indexOf(val) !== -1;
-      });
-    }
-    return res;
-  }catch(error){
-    console.error(error);
-    return;
-  }
+  return result;
 }
 
-function wkeyupCallback() {
-  var val, res, pos = parseInt($('#wplayerPos input[type=radio]:checked').val());
-  val = $('#wquesPart').val();
-  val = val.replace(/\s\s+/g, ' ');
-  $('#wresList').html('');
-  if(val.length < 2) return;
-  val = val.toLowerCase();
+function qteKeyupCallback () {
+  const val = document.querySelector('#qte_input').value.toLowerCase();
+  const pos = parseInt(document.querySelector('#qte_pos input[type=radio]:checked').value);
 
-  res = search4Result(val, dataw);
-  if(res == null) return;
-  html = '';
-  res.forEach(function(r){
-    var q = r.q, a = '';
-    for(var i = 0; i < 5; i++){
-      if(i == pos) a += '<span>' + r.a[i] + '</span>';
-      else a += r.a[i];
-    }
-    html += '<tr><td>' + a + '</td><td>' + q + '</td></tr>';
-  });
-  $('#wresList').append(html);
-}
-function ckeyupCallback() {
-  var val, res;
-  val = $('#cquesPart').val();
-  val = val.replace(/\s\s+/g, ' ');
-  $('#cresList').html('');
-  if(val.length < 2) return;
-  val = val.toLowerCase();
+  if (val.length === 0) return;
 
-  res = search4Result(val, datac);
-  if(res == null) return;
-  html = '';
-  res.forEach(function(r){
-    var q = r.q, a = '';
-    for(var i = 0; i < 5; i++){
-      if(i != 0) a += '<br />';
-      a += '<span class="cAns cAns'+ i +'">' + r.a[i] + '</span>';
-    }
-    html += '<tr><td>' + a + '</td><td>' + q + '</td></tr>';
-  });
-  $('#cresList').append(html);
+  const result = search4Result(val, qte);
+  if(result.length === 0) return;
+
+  const html = result.reduce((last, row) => {
+    const q = row.q;
+    const a = row.a.map((ai, i) => i === pos ? `<span>${ai}</span>` : `${ai}`);
+
+    last += `<tr><td>${a}</td><td>${q}</td></tr>`;
+    return last;
+  }, '');
+  
+  document.querySelector('#qte_result').innerHTML = html;
 }
+
+function multiKeyupCallback() {
+  const val = document.querySelector('#multi_input').value.toLowerCase();
+
+  if (val.length === 0) return;
+
+  const result = search4Result(val, multi);
+  if(result.length === 0) return;
+
+  const html = result.reduce((last, row) => {
+    const q = row.q;
+    const a = row.a.map((ai, i) => `<span class="cAns cAns${i}">${ai}</span>`);
+
+    last += `<tr><td>${a.join('<br />')}</td><td>${q}</td></tr>`;
+    return last;
+  }, '');
+  
+  document.querySelector('#multi_result').innerHTML = html;
+}
+
+document.querySelector('#qte_input_clear').addEventListener('click', () => {
+  document.querySelector('#qte_input').value = '';
+  document.querySelector('#qte_result').innerHTML = '';
+});
+
+document.querySelector('#multi_input_clear').addEventListener('click', () => {
+  document.querySelector('#multi_input').value = '';
+  document.querySelector('#multi_result').innerHTML = '';
+});
